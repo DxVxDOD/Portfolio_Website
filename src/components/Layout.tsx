0d587818b007a24e0240f5d4d246e0356693538d
davidOrbang <orbangdavid@yahoo.com>
2023-06-29 16:36:26 +0300

    mesh animation delleted from main branch

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,10 @@
 import React, { Suspense } from 'react'
-import AnimationCanvas from './animation/AnimationCanvas'
 
 const Layout = () => {
   return (
     <div
     className='flex shadow-layoutBoxShadow w-full h-full border-[1.25px] border-zinc-950 lg:border-[1.35px] dark:border-zinc-200'
     >
-      <Suspense fallback={<>Loading...</>} >
-        <AnimationCanvas/>
-      </Suspense>
     </div>
   )
 }
