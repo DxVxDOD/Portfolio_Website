365ea636e38ed1f937d21815142391e8923c4e4f
DxVxDOD <orbangdavid@yahoo.com>
2023-09-29 19:27:23 +0300

    deleted old design, base new design

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import React,{ useState } from 'react'
-import Layout from './components/Layout'
-import ThemeButton from './components/ThemeButton'
-import './styles/themeButton.css'
-import NavSection from './components/NavSection'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Projects from './components/Projects'
-import About from './components/About'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div
-    className='box-border flex flex-col h-screen gap-4 p-12 m-0 overflow-hidden bg-zinc-200 text-zinc-950 dark:text-zinc-200 dark:bg-zinc-950'
-    >
-      <div className='flex justify-center gap-4 lg:justify-between' >
-        <NavSection/>
-        <ThemeButton/>
-      </div>
-      <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/projects' element={<Projects/>} />
-          <Route path='/about' element={<About/>} />
-      </Routes>
-      <Layout/>
-    </div>
+      <main className={'bg-zinc-950 w-screen h-screen overflow-hidden box-border grid grid-cols-2 grid-rows-2 m-0 p-20 text-zinc-300'}>
+          <h1 className={'flex flex-col text-5xl'} >
+              <p className={'w-fit'} >David Orbang</p>
+              <p className={'p-10 pl-20 w-fit'}>Front-End Developer</p>
+          </h1>
+          <nav className={'flex gap-4 justify-end p-4 text-zinc-400'} >
+              <a href={'https://github.com/DxVxDOD'} >GitHub</a>
+              <a href={'https://www.linkedin.com/in/david-orbang/'} >LinkedIn</a>
+              <a href={'https://twitter.com/OrbanDavid24'} >Twitter</a>
+              <a href={'https://my-blog-jtqt0npju-dxvxdod.vercel.app/'} >Personal-Blog</a>
+          </nav>
+          <aside>
+              Need to write intro about me....
+          </aside>
+          <section className={'p-4'} >
+              <nav className={'flex flex-col items-end'} >
+                  <h2 className={'text-xl text-zinc-400'}>Projects:</h2>
+                  <p>Marks</p>
+                  <p>Personal-Blog</p>
+                  <p>E-Commerce-Website</p>
+              </nav>
+          </section>
+      </main>
   )
 }
 
