6f252f17a9c8bb7d43b722d0620fcb528281be19
DxVxDOD <orbangdavid@yahoo.com>
2023-10-07 11:51:13 +0300

    portfolio done until projects are finished

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,12 @@ function App() {
   return (
     <main
       className={
-        "bg-[#0D0E0E] w-screen h-screen overflow-hidden box-border flex flex-col justify-between xl:text-xl text-md " +
-        "sm:grid sm:grid-cols-2 sm:grid-rows-2 m-0 xl:p-20 p-6 text-[#dadddd]"
+        "bg-[#0D0E0E] w-screen h-screen overflow-hidden box-border flex flex-col justify-between xl:text-lg 2xl:text-xl text-md sm:grid sm:p-10 sm:grid-cols-2 sm:grid-rows-2 m-0 xl:p-20 p-6 text-[#dadddd]"
       }
     >
       <h1
         id={"name"}
-        className={"flex flex-col text-3xl md:text-5xl xl:text-7xl"}
+        className={"flex flex-col text-3xl md:text-4xl xl:text-5xl 2xl:text-7xl"}
       >
         <p className={"w-fit"}>David Orbang</p>
         <p className={"xl:p-10 pl-6 xl:pl-20 w-fit"}>Front-End Developer</p>
