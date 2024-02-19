58879d06a50c7ebdaa94a7376d94623cee4abde2
David Orbang <orbangdavid@yahoo.com>
2024-02-19 14:25:30 +0000

    edited 2nd project name

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,10 @@ export default function Projects() {
     >
       <h2 className={"text-xl text-[#939b9b] p-2"}>Projects:</h2>
       <SocialLink href="https://marks.onrender.com" title="Marks" />
-      <SocialLink href="https://inet-clone.vercel.app/" title="Inet Clone" />
+      <SocialLink
+        href="https://inet-clone.vercel.app/"
+        title="Inet Clone (In progress)"
+      />
     </nav>
   );
 }
