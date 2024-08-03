50d34275c7c9d1e35f00193b41241b05d3535725
DxVxDOD <orbangdavid@yahoo.com>
2024-08-03 18:57:21 +0300

    data display

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,6 +10,10 @@ export default function Projects() {
       <h2 className={"text-xl text-[#939b9b] p-2"}>Projects:</h2>
       <SocialLink href="https://marks.onrender.com" title="Marks" />
       <SocialLink
+        href="https://github.com/DxVxDOD/Data_table_display"
+        title="Data display exercise"
+      />
+      <SocialLink
         href="https://inet-clone.vercel.app/"
         title="Inet Clone (In progress)"
       />
